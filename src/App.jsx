15c69cb7147067fc93f3f29dfb026b009229377a
TodTheMatsu <gokudollmeanak@gmail.com>
2024-11-27 12:19:13 +0700

    style changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { motion } from 'framer-motion';
-
+import reactsvg from "./assets/react.svg"
 function App() {
   const links = [
     {
@@ -15,10 +15,10 @@ function App() {
   return (
     <>
       <div className="bg-black h-[400vh] w-full absolute flex flex-col items-center">
-        <div className="w-[1000px] h-[500px] mx-auto mt-56 flex flex-col items-center justify-center">
+        <div className="w-[1000px] h-[500px] mx-auto my-56 flex flex-col items-center justify-center">
           <motion.h1
             initial={{ opacity: 0 }}
-            animate={{ opacity: 1, transition: { delay: 1.5 } }}
+            animate={{ opacity: 1, transition: { delay: 1.2 } }}
             className="text-white font-sans font-bold mx-auto text-center w-full text-6xl blur-2xl"
           >
             Hello my name is Lee.
@@ -35,12 +35,35 @@ function App() {
             Hello my name is Lee.
           </motion.h1>
         </div>
-        <motion.div
+        <div className="w-full flex flex-wrap items-center justify-center gap-2">
+          <div  className="w-[600px] h-[400px] bg-white rounded-md backdrop-blur-md bg-opacity-20">
+            <h1 className='text-white font-sans font-thin mx-auto text-center w-full text-4xl mt-5 absolute'>About me</h1>
+            <h1 className='text-white font-sans font-bold mx-auto text-center w-full text-4xl mt-5 blur-lg'>About me</h1>
+            <p className='text-white font-sans px-5 font-thin mx-auto text-center w-full text-2xl mt-5'>I am an aspiring web developer with a strong passion for learning and growth.<br/>
+             My journey began with over four years of experience in game development, where I honed my programming skills and creative problem-solving abilities.<br/> 
+             Recently, I transitioned into web development, bringing with me a solid foundation in programming and a commitment to mastering this exciting field.
+             </p>
+          </div>
+          <div  className="w-[1250px] h-[400px] bg-white rounded-md backdrop-blur-md bg-opacity-20">
+            <h1 className='text-white font-sans font-thin mx-auto text-center w-full text-4xl mt-5 absolute'>My techstack</h1>
+            <h1 className='text-white font-sans font-bold mx-auto text-center w-full text-4xl mt-5 blur-lg'>My techstack</h1>
+            <reactsvg />
+          </div>
+          <div  className="w-1/2 h-[500px] bg-white rounded-md backdrop-blur-md bg-opacity-20">
+            <h1 className='text-white font-sans font-thin mx-auto text-center w-full text-4xl mt-5 absolute'>Task managing tool</h1>
+            <h1 className='text-white font-sans font-bold mx-auto text-center w-full text-4xl mt-5 blur-lg'>Task managing tool</h1>
+          </div>
+          <div  className="w-[900px] h-[500px] bg-white rounded-md backdrop-blur-md bg-opacity-20">
+            <h1 className='text-white font-sans font-thin mx-auto text-center w-full text-4xl mt-5 absolute'>Weather app</h1>
+            <h1 className='text-white font-sans font-bold mx-auto text-center w-full text-4xl mt-5 blur-lg'>Weather app</h1>
+          </div>
+        </div>
+        <motion.div // The div that holds the links
           initial={{ opacity: 0, width: "60px", y: 100 }}
           animate={{
             opacity: 1,
             y: 0,
-            width: ["60px", "60px", "600px"],
+            width: ["60px", "60px", "300px"],
           }}
           transition={{
             opacity: { duration: 0.5 },
@@ -48,16 +71,15 @@ function App() {
             width: { delay: 3, duration: 2 },
             ease: "easeInOut",
           }}
-          className="w-[600px] h-[60px] fixed bg-white rounded-full backdrop-blur-md bg-opacity-20 top-[850px] flex items-center justify-center space-x-9"
+          className="h-[60px] fixed bg-white rounded-full backdrop-blur-md bg-opacity-20 top-[850px] flex items-center justify-center space-x-9"
         >
           {links.map((link, index) => (
-        
             <motion.a
               key={index}
               initial={{ opacity: 0 }}
               animate={{ opacity: 1 }}
               transition={{
-                delay: 5 + index * 0.2, // Stagger the delays for each link
+                delay: 5 + index * 0.2,
                 duration: 0.5,
               }}
               target="_blank"
