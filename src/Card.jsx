4d3441f4bd90a4f9e0c85c99d51e01558628f657
TodTheMatsu <gokudollmeanak@gmail.com>
2024-12-02 12:31:53 +0700

    more oor

--- src/Card.jsx
+++ src/Card.jsx
@@ -4,7 +4,6 @@ function Card({image}) {
     return (
         <motion.div initial={{ rotate: randomRotation}} whileHover={{ rotate: 0, scale: 1.1}} whileTap={{scale:1.2}} className='bg-white w-[300px] h-[400px] rounded-2xl pointer-events-auto cursor-pointer'>
             <motion.img src={image} className=' object-cover w-full h-full object-center rounded-2xl'>
-
             </motion.img>
         </motion.div>
     )
