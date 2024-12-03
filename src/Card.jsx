59a8c8037af195c2ee40e8b8f11db7e551729e4e
TodTheMatsu <gokudollmeanak@gmail.com>
2024-12-03 13:05:48 +0700

    fixed the info closing even though the background isn't clicked on

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,9 +1,10 @@
 import { motion } from 'framer-motion'
 function Card({ image, info, onClick }) {
-    const randomRotation = Math.random() * 20 - 10
+    const randomRotation = Math.random() < 0.5 ? -10 : 10;
+
     return (
         <>
-        <motion.div  onClick={onClick} initial={{ rotate: randomRotation}} whileHover={{ rotate: 0, scale: 1.1}} whileTap={{scale:1.2}} className='bg-white w-[300px] h-[400px] rounded-2xl pointer-events-auto cursor-pointer'>
+        <motion.div  onClick={onClick} initial={{ rotate: randomRotation}} whileHover={{ rotate: 0, scale: 1.1}} whileTap={{scale:1.4}} className='bg-white w-[300px] h-[400px] rounded-2xl pointer-events-auto cursor-pointer shadow-2xl'>
             <motion.img src={image} className=' object-cover w-full h-full object-center rounded-2xl'>
             </motion.img>
         </motion.div>
--- src/WhattodoInfo.jsx
+++ src/WhattodoInfo.jsx
@@ -1,19 +1,20 @@
-import {motion} from 'framer-motion'
-function WhattodoInfo({onClick}) {
-    return (
-        <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }} onClick={onClick} className='w-screen h-screen bg-black bg-opacity-50 backdrop-blur-md flex-col flex justify-center items-center fixed'>
-            <motion.div className='w-full h-full flex justify-center overflow-auto py-28 space-x-5 px-20 scroll-smooth '>
-                <motion.div className='w-1/2 h-[2000px] bg-white bg-opacity-20 rounded-2xl flex flex-col justify-center items-center backdrop-blur-md'>
-                </motion.div>
-                <motion.div className='w-[200px] right-[15%] bg-white bg-opacity-20 rounded-3xl flex flex-col gap-5 py-5 justify-center items-center fixed backdrop-blur-md '>
-                    <motion.h1 className='text-white font-sans font-thin mx-auto text-center w-full text-2xl cursor-pointer'>Introduction</motion.h1>
-                    <motion.h1 className='text-white font-sans font-thin mx-auto text-center w-full text-2xl cursor-pointer'>Usage</motion.h1>
-                    <motion.h1 className='text-white font-sans font-thin mx-auto text-center w-full text-2xl cursor-pointer'>Functions</motion.h1>
-                </motion.div>
-            </motion.div>
-        </motion.div>
-        
-    )
+import { motion } from 'framer-motion';
+
+function WhattodoInfo({ onClick }) {
+  const handleClick = (e) => { console.log(e.target); console.log(e.currentTarget); if (e.target === e.currentTarget) { onClick(); } };
+
+  return (
+  <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }} 
+  className="w-screen h-screen bg-black bg-opacity-50 backdrop-blur-md flex-col flex justify-center items-center fixed">
+    <motion.div onClick={handleClick}  className="w-full h-full flex justify-center overflow-auto py-28 space-x-5 px-20 scroll-smooth">
+      <motion.div className="w-1/2 h-[2000px] bg-white bg-opacity-20 rounded-2xl flex flex-col justify-center items-center backdrop-blur-md" />
+      <motion.div className="w-[200px] right-[15%] bg-white bg-opacity-20 rounded-3xl flex flex-col gap-5 py-5 justify-center items-center fixed backdrop-blur-md">
+        <motion.h1 className="text-white font-sans font-thin mx-auto text-center w-full text-2xl cursor-pointer">Introduction</motion.h1>
+        <motion.h1 className="text-white font-sans font-thin mx-auto text-center w-full text-2xl cursor-pointer">Usage</motion.h1>
+        <motion.h1 className="text-white font-sans font-thin mx-auto text-center w-full text-2xl cursor-pointer">Functions</motion.h1>
+      </motion.div>
+    </motion.div>
+  </motion.div>)
 }
 
-export default WhattodoInfo
+export default WhattodoInfo;
