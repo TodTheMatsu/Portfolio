b73316b81d605f99a7ba4a4667e7f651bfeabe55
TodTheMatsu <gokudollmeanak@gmail.com>
2024-12-05 13:01:48 +0700

    the side panel now works!

--- src/WhattodoInfo.jsx
+++ src/WhattodoInfo.jsx
@@ -1,4 +1,5 @@
 import { motion } from 'framer-motion';
+import { useRef } from 'react';
 import vid1 from './assets/videos/whattodo/vid1.mp4';
 import vid2 from './assets/videos/whattodo/vid2.mp4';
 import vid3 from './assets/videos/whattodo/vid3.mp4';
@@ -6,12 +7,24 @@ import vid4 from './assets/videos/whattodo/vid4.mp4';
 import vid5 from './assets/videos/whattodo/vid5.mp4';
 import vid6 from './assets/videos/whattodo/vid6.mp4';
 import vid7 from './assets/videos/whattodo/vid7.mp4';
+import { s } from 'motion/react-client';
 
 function WhattodoInfo({ onClick }) {
   const handleClick = (e) => {
     if (e.target === e.currentTarget) onClick();
   };
-
+  const introRef = useRef(null);
+  const featuresRef = useRef(null);
+  const techStackRef = useRef(null);
+  const scrollToSection = (ref) => {
+    console.log(ref);
+    ref.current?.scrollIntoView({ behavior: 'smooth' });
+  };
+  const sectionRefs = {
+    Introduction: introRef,
+    Features: featuresRef,
+    'Technology Stack': techStackRef,
+  };
   const description = 'A dynamic and interactive task management system that allows users to manage tasks through boards. The app features drag-and-drop functionality to reorder boards and tasks, supports dark mode, and integrates with a backend for storing board data.';
   const tech = {
     Frontend: [
@@ -106,7 +119,7 @@ function WhattodoInfo({ onClick }) {
             What to do?
           </motion.h1>
           <motion.hr variants={textVariants} className="w-full mx-auto border-dashed" />
-          <motion.h1 variants={textVariants} className="text-white font-sans font-thin mx-auto text-center rounded-3xl px-2 outline-dashed pb-2 text-4xl">
+          <motion.h1 variants={textVariants} ref={introRef} className="text-white font-sans font-thin mx-auto text-center rounded-3xl px-2 outline-dashed pb-2 text-4xl">
             Introduction
           </motion.h1>
           <motion.video variants={textVariants} viewport={{ once: true }} autoPlay loop className="w-full h-auto rounded-2xl shadow-2xl">
@@ -116,7 +129,7 @@ function WhattodoInfo({ onClick }) {
             {renderText(description)}
           </motion.p>
           <motion.hr variants={textVariants} className="w-full mx-auto border-dashed" />
-          <motion.h1 variants={textVariants} className="text-white font-sans font-thin mx-auto text-center rounded-3xl px-2 outline-dashed pb-2 text-4xl">
+          <motion.h1 variants={textVariants} ref={featuresRef} className="text-white font-sans font-thin mx-auto text-center rounded-3xl px-2 outline-dashed pb-2 text-4xl">
             Features
           </motion.h1>
           {features.map((feature, index) => (
@@ -133,7 +146,7 @@ function WhattodoInfo({ onClick }) {
             </div>
           ))}
           <motion.hr variants={textVariants} className="w-full mx-auto flex items-center justify-center border-dashed" />
-          <motion.h1 variants={textVariants} className="text-white font-sans font-thin mx-auto text-center rounded-3xl px-4 outline-dashed pb-2 text-4xl">
+          <motion.h1 variants={textVariants} ref={techStackRef} className="text-white font-sans font-thin mx-auto text-center rounded-3xl px-4 outline-dashed pb-2 text-4xl">
             Technology Stack
           </motion.h1>
           <motion.div className='flex items-center justify-center border-dashedflex-row gap-5'>
@@ -164,11 +177,17 @@ function WhattodoInfo({ onClick }) {
           </motion.a>
         </motion.div>
         <motion.div className="w-[200px] right-[15%] bg-white bg-opacity-20 rounded-3xl flex flex-col gap-5 py-5 justify-center items-center fixed backdrop-blur-md">
-          {['Introduction', 'Features', 'Functions'].map((section) => (
-            <motion.h1 key={section} className="text-white font-sans font-thin mx-auto text-center w-full text-2xl cursor-pointer">
-              {section}
-            </motion.h1>
-          ))}
+          {Object.entries(sectionRefs).map(([section, ref]) => {
+            return (
+              <motion.h1
+                key={section}
+                onClick={() => scrollToSection(ref)}
+                className="text-white font-sans font-thin mx-auto text-center w-full text-2xl cursor-pointer"
+              >
+                {section}
+              </motion.h1>
+            );
+          })}
         </motion.div>
       </motion.div>
     </motion.div>
