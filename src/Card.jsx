8fd24ed83b5575417a541cf8953a45f14fe00f17
TodTheMatsu <gokudollmeanak@gmail.com>
2025-02-23 21:11:15 +0700

    minor changes

--- src/Card.jsx
+++ src/Card.jsx
@@ -2,11 +2,10 @@ import { motion } from 'framer-motion';
 import { useState } from 'react';
 
 function Card({ image, info, onClick, index }) {
-    const [isHovered, setIsHovered] = useState(false);
     const randomRotation = Math.random() < 0.5 ? -10 : 10;
     
     return (
-        <motion.button  data-cursor-text="View" data-cursor-size="80px"
+        <motion.button  data-cursor-text="VIEW" data-cursor-size="130px"
             onClick={onClick} 
             initial={{ opacity: 0, rotate: randomRotation, x: '100%' }} 
             whileInView={{ rotate: randomRotation, x: 0, opacity: 1,
@@ -17,8 +16,6 @@ function Card({ image, info, onClick, index }) {
                 }
             }} 
             exit={{ x: '-100%', opacity: 0 }}
-            onMouseEnter={() => setIsHovered(true)}
-            onMouseLeave={() => setIsHovered(false)}
             whileHover={{ rotate: 0, scale: 1.2, transition: { duration: 0.2 } }} 
             whileTap={{ 
                 scale: 1.3, 
@@ -28,18 +25,7 @@ function Card({ image, info, onClick, index }) {
                 } 
             }} 
             className='relative card  bg-white lg:w-[300px] lg:h-[400px] md:w-[250px] md:h-[350px] sm:w-[150px] sm:h-[200px] w-[100px] h-[150px] rounded-2xl pointer-events-auto cursor-pointer shadow-2xl overflow-hidden'>
-            
             <motion.img src={image} className='object-cover w-full h-full object-center rounded-2xl' />
-            
-            {isHovered && (
-                <motion.div 
-                    initial={{ opacity: 0 }} 
-                    animate={{ opacity: 1 }} 
-                    exit={{ opacity: 0 }} 
-                    className='absolute inset-0 flex items-center justify-center bg-black bg-opacity-50 rounded-2xl'>
-                    <p className='text-white text-lg font-bold'>View Project</p>
-                </motion.div>
-            )}
         </motion.button>
     );
 }
